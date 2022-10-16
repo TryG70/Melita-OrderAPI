54f8e2a294a26a5b87de8d3d66514ed639d0eee5
TG <trygod.nwakwasi@decagon.dev>
2022-10-17 00:34:36 +0100

    added mail service

--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/listener/OrderListener.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/listener/OrderListener.java
@@ -4,23 +4,28 @@ import com.melita.orderfulfillmentapi.configuration.MQConfig;
 import com.melita.orderfulfillmentapi.entity.Order;
 import com.melita.orderfulfillmentapi.response.OrderResponse;
 import com.melita.orderfulfillmentapi.service.OrderService;
+import com.melita.orderfulfillmentapi.serviceImpl.MailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
 
 @Component
 public class OrderListener {
 
     private final OrderService orderService;
+    private final MailService mailService;
 
     @Autowired
-    public OrderListener(OrderService orderService) {
+    public OrderListener(OrderService orderService, MailService mailService) {
         this.orderService = orderService;
+        this.mailService = mailService;
     }
 
 
     @RabbitListener(queues = MQConfig.ORDER_QUEUE)
-    public void listen(OrderResponse orderResponse) {
+    public void listen(OrderResponse orderResponse) throws MessagingException {
 
         Order order = Order.builder()
                 .customerName(orderResponse.getCustomerName())
@@ -32,5 +37,17 @@ public class OrderListener {
                 .build();
 
         orderService.fulfillOrder(order);
+
+        String email = orderResponse.getCustomerEmail();
+        String mailSubject = "Order Details";
+        String emailBody = "Kindly find the details of the Customer Order"  + "\n"
+                + "Customer Name: " + orderResponse.getCustomerName() + "\n"
+                + "Customer Email: " + orderResponse.getCustomerEmail() + "\n"
+                + "Customer Installation Address: " + orderResponse.getInstallationAddress() + "\n"
+                + "Customer Installation Date: " + orderResponse.getInstallationDates() + "\n"
+                + "Customer Installation Date: " + orderResponse.getProduct() + "\n"
+                + "Customer Installation Date: " + orderResponse.getProductPackage() + "\n";
+
+        mailService.sendEmail(email, mailSubject, emailBody);
     }
 }
