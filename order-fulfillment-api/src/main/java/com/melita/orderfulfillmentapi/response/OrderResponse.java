662ec7a0787c58a71b1d2b9b784266ecc247c9a7
TG <trygod.nwakwasi@decagon.dev>
2022-10-17 00:35:58 +0100

    implemented serializable and getter and setter

--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/response/OrderResponse.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/response/OrderResponse.java
@@ -1,18 +1,23 @@
 package com.melita.orderfulfillmentapi.response;
 
-import lombok.Builder;
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 
 @Builder
 @Data
-public class OrderResponse {
+@Getter
+@Setter
+public class OrderResponse implements Serializable {
 
     private String customerName;
     private String customerEmail;
     private String installationAddress;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH")
     private LocalDateTime installationDates;
     private String product;
     private String productPackage;
