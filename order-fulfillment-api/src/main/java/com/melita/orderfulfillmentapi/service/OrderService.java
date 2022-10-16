3d4bbbff2a4c56629dd13abd6339ecd8419a1000
TG <trygod.nwakwasi@decagon.dev>
2022-10-16 05:21:30 +0100

    added a method and its implementation

--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/service/OrderService.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/service/OrderService.java
@@ -1,4 +1,8 @@
 package com.melita.orderfulfillmentapi.service;
 
+import com.melita.orderfulfillmentapi.entity.Order;
+
 public interface OrderService {
+
+    void fulfillOrder(Order order);
 }
--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/serviceImpl/OrderServiceImpl.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/serviceImpl/OrderServiceImpl.java
@@ -1,9 +1,24 @@
 package com.melita.orderfulfillmentapi.serviceImpl;
 
+import com.melita.orderfulfillmentapi.entity.Order;
+import com.melita.orderfulfillmentapi.repository.OrderRepository;
 import com.melita.orderfulfillmentapi.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OrderServiceImpl implements OrderService {
 
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public void fulfillOrder(Order order) {
+
+        orderRepository.save(order);
+    }
 }
