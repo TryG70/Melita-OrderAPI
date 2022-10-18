eb74ccb8d3391207c988f5463d9e9ebb4850e6a4
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 02:36:06 +0100

    created the OrderResponse class for order-approval module

---
+++ order-approval-api/src/main/java/com/melita/orderapprovalapi/response/OrderResponse.java
@@ -0,0 +1,28 @@
+package com.melita.orderapprovalapi.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+
+@Builder
+@Data
+@Getter
+@Setter
+public class OrderResponse implements Serializable {
+
+    private String customerName;
+    private String customerEmail;
+    private String installationAddress;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH")
+    private LocalDateTime installationDates;
+    private String product;
+    private String productPackage;
+    private String isApproved;
+}
