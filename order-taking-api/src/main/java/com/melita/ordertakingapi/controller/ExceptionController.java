2160c7d7e33e635ac5098fa7729654d3bb0c4916
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 07:48:34 +0100

    added a new method(notAuthorized) to ExceptionController in order-taking module

--- order-taking-api/src/main/java/com/melita/ordertakingapi/controller/ExceptionController.java
+++ order-taking-api/src/main/java/com/melita/ordertakingapi/controller/ExceptionController.java
@@ -1,6 +1,7 @@
 package com.melita.ordertakingapi.controller;
 
 import com.melita.ordertakingapi.exception.InvalidProductException;
+import com.melita.ordertakingapi.exception.NotAuthorizedException;
 import com.melita.ordertakingapi.response.ExceptionResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 
 @ControllerAdvice
@@ -19,4 +21,13 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
         ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), FORBIDDEN);
         return new ResponseEntity<>(exceptionResponse, FORBIDDEN);
     }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<Object> notAuthorized(NotAuthorizedException exception) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), UNAUTHORIZED);
+        return new ResponseEntity<>(exceptionResponse, UNAUTHORIZED);
+    }
+
+
 }
