b2a840326f0bbbe428d49439014e62c7255ccf94
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 02:34:28 +0100

    created an exception class for order-fulfillment module

---
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/exception/OrderNotApprovedException.java
@@ -0,0 +1,8 @@
+package com.melita.orderfulfillmentapi.exception;
+
+public class OrderNotApprovedException extends RuntimeException {
+    public OrderNotApprovedException(String message) {
+
+    }
+
+}
