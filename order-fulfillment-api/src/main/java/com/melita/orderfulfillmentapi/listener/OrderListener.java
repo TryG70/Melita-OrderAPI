b57d769deae2074a8c13223beaf89b36f7b0969e
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 07:51:38 +0100

    refactored

--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/listener/OrderListener.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/listener/OrderListener.java
@@ -1,11 +1,13 @@
 package com.melita.orderfulfillmentapi.listener;
 
-import com.melita.orderfulfillmentapi.configuration.MQConfig;
+import com.melita.orderfulfillmentapi.configuration.ApprovalMQConfig;
+import com.melita.orderfulfillmentapi.configuration.MailMQConfig;
 import com.melita.orderfulfillmentapi.entity.Order;
 import com.melita.orderfulfillmentapi.exception.OrderNotApprovedException;
 import com.melita.orderfulfillmentapi.response.OrderResponse;
 import com.melita.orderfulfillmentapi.service.OrderService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,13 +17,16 @@ public class OrderListener {
 
     private final OrderService orderService;
 
+    private final RabbitTemplate rabbitTemplate;
+
     @Autowired
-    public OrderListener(OrderService orderService) {
+    public OrderListener(OrderService orderService, RabbitTemplate rabbitTemplate) {
         this.orderService = orderService;
+        this.rabbitTemplate = rabbitTemplate;
     }
 
 
-    @RabbitListener(queues = MQConfig.ORDER_APPROVAL_QUEUE)
+    @RabbitListener(queues = ApprovalMQConfig.ORDER_APPROVAL_QUEUE)
     public void listen(OrderResponse orderResponse) {
 
         if (orderResponse.getIsApproved().equals("true")) {
@@ -38,22 +43,10 @@ public class OrderListener {
 
             orderService.fulfillOrder(order);
 
+            rabbitTemplate.convertAndSend(MailMQConfig.ORDER_MAIL_EXCHANGE, MailMQConfig.ORDER_MAIL_ROUTING_KEY, orderResponse);
+
         } else {
             throw new OrderNotApprovedException("Order not approved");
         }
-
-
-
-//        String email = orderResponse.getCustomerEmail();
-//        String mailSubject = "Order Details";
-//        String emailBody = "Kindly find the details of the Customer Order"  + "\n"
-//                + "Customer Name: " + orderResponse.getCustomerName() + "\n"
-//                + "Customer Email: " + orderResponse.getCustomerEmail() + "\n"
-//                + "Customer Installation Address: " + orderResponse.getInstallationAddress() + "\n"
-//                + "Customer Installation Date: " + orderResponse.getInstallationDates() + "\n"
-//                + "Customer Installation Date: " + orderResponse.getProduct() + "\n"
-//                + "Customer Installation Date: " + orderResponse.getProductPackage() + "\n";
-//
-//        mailService.sendEmail(email, mailSubject, emailBody);
     }
 }
