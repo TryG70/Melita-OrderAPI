b93f2aaa7ec7717eaf2b298a3f4fda38f14d1825
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 02:31:27 +0100

    created the OrderApprovalController class for order-approval module

---
+++ order-approval-api/src/main/java/com/melita/orderapprovalapi/controller/OrderApprovalController.java
@@ -0,0 +1,36 @@
+package com.melita.orderapprovalapi.controller;
+
+import com.melita.orderapprovalapi.configuration.ApprovalMQConfig;
+import com.melita.orderapprovalapi.service.OrderService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@AllArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("api/v1/order-approval")
+public class OrderApprovalController {
+
+    private final OrderService orderService;
+    private final RabbitTemplate rabbitTemplate;
+
+
+    @PostMapping(value = "/approved/{id}")
+    public String approveOrder(@PathVariable("id") Long id) {
+        log.info("Approve Order with Id: {}", id);
+        rabbitTemplate.convertAndSend(ApprovalMQConfig.ORDER_APPROVAL_EXCHANGE, ApprovalMQConfig.ORDER_APPROVAL_ROUTING_KEY, orderService.approveOrder(id));
+        return "Published to RabbitMQ";
+    }
+
+
+    @PostMapping(value = "/declined/{id}")
+    public String declineOrder(@PathVariable("id") Long id) {
+        log.info("Approve Order with Id: {}", id);
+        rabbitTemplate.convertAndSend(ApprovalMQConfig.ORDER_APPROVAL_EXCHANGE, ApprovalMQConfig.ORDER_APPROVAL_ROUTING_KEY, orderService.declineOrder(id));
+        return "Published to RabbitMQ";
+    }
+}
