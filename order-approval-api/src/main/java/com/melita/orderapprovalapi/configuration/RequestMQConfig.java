8014a5f736a082a3ad60b075a4651e1f8c7c1880
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 02:23:54 +0100

    created the approval module

---
+++ order-approval-api/src/main/java/com/melita/orderapprovalapi/configuration/RequestMQConfig.java
@@ -0,0 +1,55 @@
+package com.melita.orderapprovalapi.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.paranamer.ParanamerModule;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RequestMQConfig {
+
+    public static final String ORDER_QUEUE = "order-queue";
+    public static final String ORDER_EXCHANGE = "order-exchange";
+    public static final String ORDER_ROUTING_KEY = "order-routing-key";
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(ORDER_QUEUE);
+    }
+
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(ORDER_EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue orderQueue, TopicExchange orderExchange) {
+        return BindingBuilder
+                .bind(orderQueue)
+                .to(orderExchange)
+                .with(ORDER_ROUTING_KEY);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+
+        ObjectMapper mapper = new ObjectMapper().findAndRegisterModules().registerModule(new ParanamerModule());;
+
+        return new Jackson2JsonMessageConverter(mapper);
+
+    }
+
+    @Bean
+    public AmqpTemplate ampqTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+}
