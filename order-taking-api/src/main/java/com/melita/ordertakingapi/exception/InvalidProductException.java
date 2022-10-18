756d10f1277c17a96c9c2879e90081d243632f0d
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 07:42:23 +0100

    refactored the class

--- order-taking-api/src/main/java/com/melita/ordertakingapi/exception/InvalidProductException.java
+++ order-taking-api/src/main/java/com/melita/ordertakingapi/exception/InvalidProductException.java
@@ -1,8 +1,15 @@
 package com.melita.ordertakingapi.exception;
 
+import lombok.Data;
+
+@Data
 public class InvalidProductException extends RuntimeException {
 
+    private String message;
+
     public InvalidProductException(String message) {
+
+        this.message = message;
     }
 
 }
