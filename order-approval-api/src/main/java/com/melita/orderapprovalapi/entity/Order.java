6291a16dc467ce1bfa755c53dfcba7dd7cd33443
TG <trygod.nwakwasi@decagon.dev>
2022-10-18 02:30:08 +0100

    created the order entity for order approval module

---
+++ order-approval-api/src/main/java/com/melita/orderapprovalapi/entity/Order.java
@@ -0,0 +1,28 @@
+package com.melita.orderapprovalapi.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String customerName;
+    private String customerEmail;
+    private String installationAddress;
+    private LocalDateTime installationDates;
+    private String product;
+    private String productPackage;
+    private String isApproved;
+}
