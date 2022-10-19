e0209ed7bebf405bd51f61ab0bdf43a1d877f447
TG <trygod.nwakwasi@decagon.dev>
2022-10-19 02:11:29 +0100

    changed the data type of installationDates

--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/response/OrderResponse.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/response/OrderResponse.java
@@ -16,9 +16,7 @@ public class OrderResponse implements Serializable {
     private String customerName;
     private String customerEmail;
     private String installationAddress;
-
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH")
-    private LocalDateTime installationDates;
+    private String installationDates;
     private String product;
     private String productPackage;
     private String isApproved;
