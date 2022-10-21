27810778e90906a35a2b8b9858bc6f2510f48962
TG <trygod.nwakwasi@decagon.dev>
2022-10-21 15:29:57 +0100

    refactored the createOrder method

--- order-taking-api/src/main/java/com/melita/ordertakingapi/serviceImpl/OrderServiceImpl.java
+++ order-taking-api/src/main/java/com/melita/ordertakingapi/serviceImpl/OrderServiceImpl.java
@@ -13,37 +13,37 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public OrderResponse createOrder(OrderRequest orderRequest) {
+
         log.info("Order request received: {}", orderRequest);
-        OrderResponse orderResponse = OrderResponse.builder()
-                .customerName(orderRequest.getCustomerName())
-                .customerEmail(orderRequest.getCustomerEmail())
-                .installationAddress(orderRequest.getInstallationAddress())
-                .installationDates(orderRequest.getInstallationDates())
-                .product(orderRequest.getProduct())
-                .build();
+        if (orderRequest.getProduct().matches("(INTERNET_250Mbps|INTERNET_1GBps|TV_90_Channels|TV_140_Channels|TELEPHONY_Free_On_Net_Calls|TELEPHONY_Unlimited_Calls|MOBILE_Prepaid|MOBILE_Postpaid)")) {
 
-        String product = orderRequest.getProduct().toLowerCase();
+            OrderResponse orderResponse = OrderResponse.builder()
+                    .customerName(orderRequest.getCustomerName())
+                    .customerEmail(orderRequest.getCustomerEmail())
+                    .installationAddress(orderRequest.getInstallationAddress())
+                    .installationDate(orderRequest.getInstallationDate())
+                    .product(orderRequest.getProduct())
+                    .build();
 
-        switch (product) {
-            case "internet_250mbps" -> orderResponse.setProductPackage("Internet 250Mbps");
-            case "internet_1gbps" -> orderResponse.setProductPackage("Internet 1GBps");
-            case "tv_90_channels" -> orderResponse.setProductPackage("TV with 90 Channels");
-            case "tv_140_channels" -> orderResponse.setProductPackage("TV with 140 Channels");
-            case "telephony_free_on_net_calls" -> orderResponse.setProductPackage("Telephony with Free On net Calls");
-            case "telephony_unlimited_calls" -> orderResponse.setProductPackage("Telephony with Free Unlimited Calls");
-            case "mobile_prepaid" -> orderResponse.setProductPackage("Mobile Prepaid");
-            case "mobile_postpaid" -> orderResponse.setProductPackage("Mobile Postpaid");
-            default -> orderResponse.setProductPackage("Invalid Product");
+            String product = orderRequest.getProduct().toLowerCase();
+
+
+            switch (product) {
+                case "internet_250mbps" -> orderResponse.setProductPackage("Internet 250Mbps");
+                case "internet_1gbps" -> orderResponse.setProductPackage("Internet 1GBps");
+                case "tv_90_channels" -> orderResponse.setProductPackage("TV with 90 Channels");
+                case "tv_140_channels" -> orderResponse.setProductPackage("TV with 140 Channels");
+                case "telephony_free_on_net_calls" -> orderResponse.setProductPackage("Telephony with Free On net Calls");
+                case "telephony_unlimited_calls" -> orderResponse.setProductPackage("Telephony with Free Unlimited Calls");
+                case "mobile_prepaid" -> orderResponse.setProductPackage("Mobile Prepaid");
+                case "mobile_postpaid" -> orderResponse.setProductPackage("Mobile Postpaid");
+            }
+
+            return orderResponse;
+
+        } else {
+            throw new InvalidProductException("Invalid product");
         }
-
-
-        if (orderResponse.getProductPackage().equals("Invalid Product")) {
-            throw new InvalidProductException("Invalid Product");
-        }
-
-        return orderResponse;
 
     }
-
-
 }
