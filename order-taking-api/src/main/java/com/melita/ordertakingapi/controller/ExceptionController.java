80147803dc15bb05249d247760f86e712377314f
TG <trygod.nwakwasi@decagon.dev>
2022-10-21 14:20:39 +0100

    changed the HttpStatus of invalidProduct method

--- order-taking-api/src/main/java/com/melita/ordertakingapi/controller/ExceptionController.java
+++ order-taking-api/src/main/java/com/melita/ordertakingapi/controller/ExceptionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static org.springframework.http.HttpStatus.FORBIDDEN;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 
 
@@ -18,8 +18,8 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
     @ExceptionHandler(InvalidProductException.class)
     public ResponseEntity<Object> invalidProduct(InvalidProductException exception) {
 
-        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), FORBIDDEN);
-        return new ResponseEntity<>(exceptionResponse, FORBIDDEN);
+        ExceptionResponse exceptionResponse = new ExceptionResponse(exception.getMessage(), BAD_REQUEST);
+        return new ResponseEntity<>(exceptionResponse, BAD_REQUEST);
     }
 
     @ExceptionHandler(NotAuthorizedException.class)
