add78567721c2702add4c7be0567bc9d94c780db
TG <trygod.nwakwasi@decagon.dev>
2022-10-21 15:37:10 +0100

    refactored the class

--- order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/exception/OrderNotApprovedException.java
+++ order-fulfillment-api/src/main/java/com/melita/orderfulfillmentapi/exception/OrderNotApprovedException.java
@@ -5,9 +5,8 @@ import lombok.Data;
 @Data
 public class OrderNotApprovedException extends RuntimeException {
 
-    private String message;
     public OrderNotApprovedException(String message) {
-        this.message = message;
+        super(message);
     }
 
 }
